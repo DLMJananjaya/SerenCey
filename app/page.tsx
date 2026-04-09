bdb2300cb76612972553fca07ed013c2eb415120
Matheesha Jananjaya <matheeshajananjaya2@gmail.com>
2026-04-09 06:34:39 +0530

    image case eka

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,17 @@ import Link from 'next/link';
 import Login from './login/page';
 import Footer from './components/Footer';
 
+const base = process.env.NODE_ENV === 'production' ? '/SerenCey' : '';
+
 const images = [
-  "/hero/1.jpg",
-  "/hero/13.jpg",
-  "/hero/16.jpeg",
-  "/hero/17.jpeg",
-  "/hero/21.jpeg",
-  "/hero/47.jpg"
-  ];
+  `${base}/hero/1.jpg`,
+  `${base}/hero/13.jpg`,
+  `${base}/hero/16.jpeg`,
+  `${base}/hero/17.jpeg`,
+  `${base}/hero/21.jpeg`,
+  `${base}/hero/47.jpg`
+];
+
 
 export default function HeroSlideshow() {
   const [index, setIndex] = useState(0);
