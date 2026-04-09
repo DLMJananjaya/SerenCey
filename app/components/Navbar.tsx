7b43d03d9d4e46640dd3667ad606bf1fd906effc
Matheesha Jananjaya <matheeshajananjaya2@gmail.com>
2026-04-09 06:57:53 +0530

    logo problem

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,8 @@ import { Menu, X } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 
-const base = process.env.NODE_ENV === 'production' ? '/SerenCey' : '';
+
+import logoImg from '../../public/Untitled_design__1_-removebg-preview.png'; // Adjust the path based on your folder
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,7 +21,7 @@ const Navbar = () => {
         <div className="flex-shrink-0">
           <Link href="/" className="flex items-center">
             <Image
-              src={`${base}/Untitled_design__1_-removebg-preview.png`} 
+              src={logoImg} 
               alt="SerenCey Logo"
               width={400} 
               height={400} 
